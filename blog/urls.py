50e71d9a3694d3043df9d81776712e1582457bae
zakpug <zakpug@redteam.com>
2024-07-06 10:24:56 -0400

    dfeasgaeta

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
-     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
